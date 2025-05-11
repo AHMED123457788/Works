b05c043f7e1c02d60b2a4b8d84e1b2a12487aba2
AHMED123457788 <103061659+AHMED123457788@users.noreply.github.com>
2025-05-11 10:45:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from telegram import Bot, Update
 from telegram.ext import Dispatcher, MessageHandler, Filters, CallbackContext
 import os
+import re
 
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = Bot(token=TOKEN)
@@ -16,12 +17,24 @@ def home():
 def webhook():
     update = Update.de_json(request.get_json(force=True), bot)
     dp = Dispatcher(bot, None, workers=0)
-    dp.add_handler(MessageHandler(Filters.text & Filters.regex("moto"), send_video))
+
+    # الكلمات المدعومة
+    pattern = re.compile(r'\b(moto|funny|brazil|gun|cazy)\b', re.IGNORECASE)
+    dp.add_handler(MessageHandler(Filters.text & Filters.regex(pattern), send_video))
+    
     dp.process_update(update)
     return 'ok'
 
 def send_video(update: Update, context: CallbackContext):
-    update.message.reply_video("https://t.me/a_moment_before/8014", caption="مرحباً! هذا هو الفيديو الخاص بك.")
+    chat_id = update.message.chat_id
+    videos = [
+        "https://t.me/archive_360/3",
+        "https://t.me/archive_360/4",
+        "https://t.me/daily_619/66"
+    ]
+    # إرسال كل فيديو واحدًا تلو الآخر
+    for video in videos:
+        bot.send_video(chat_id=chat_id, video=video)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
